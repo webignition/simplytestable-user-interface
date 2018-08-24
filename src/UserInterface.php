8f199f7302ce935c4cc6579f68608d181b32694d
Jon Cram <jon@webignition.net>
2018-08-24 18:16:50 +0100

    Allow null to be returned by getUsername(), getPassword()

--- src/UserInterface.php
+++ src/UserInterface.php
@@ -5,8 +5,8 @@ namespace webignition\SimplyTestableUserInterface;
 interface UserInterface
 {
     public function setUsername(string $username);
-    public function getUsername(): string;
+    public function getUsername(): ?string;
     public function setPassword(string $password);
-    public function getPassword(): string;
+    public function getPassword(): ?string;
     public function equals(UserInterface $user): bool;
 }
