f71d6996d7a917d3174392582256fee1defa2ed6
Jon Cram <jon@webignition.net>
2018-08-24 17:56:32 +0100

    Import

---
+++ src/UserInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace webignition\SimplyTestableUserInterface;
+
+interface UserInterface
+{
+    public function setUsername(string $username);
+    public function getUsername(): string;
+    public function setPassword(string $password);
+    public function getPassword(): string;
+    public function equals(UserInterface $user): bool;
+}
